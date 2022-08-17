3cae2e285e50f71fc6b3992866cab784cb499178
potter-18 <96332841+potter-18@users.noreply.github.com>
2022-08-18 00:21:16 +0530

    Time: 3 ms (76.26%), Space: 9.2 MB (30.17%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> total;
+        nums1.resize(m);
+        int gap = ceil((m+n)/2);
         
-        
-        for(int i = 0; i < m; i++){
-            total.push_back(nums1[i]);
+        while(gap > 0){
+            int i = 0; 
+            int j = gap;
+            
+            while(j < (m+n)){
+                if(j < m && nums1[i] > nums1[j])
+                    swap(nums1[i],nums1[j]);   
+                else if(i < m && j >= m && nums1[i] > nums2[j-m])
+                    swap(nums1[i],nums2[j-m]);   
+                else if(i >= m && j >= m && nums2[i-m] > nums2[j-m])
+                    swap(nums2[i-m],nums2[j-m]);
+                
+                i++;
+                j++;
+                
+            }
+            if(gap == 1)
+                gap = 0;
+            else
+                gap = ceil((double)gap/2);
         }
+        
         for(auto it:nums2){
-            total.push_back(it);
+            nums1.push_back(it);
         }
         
-        sort(total.begin(),total.end());
-        
-        for(int i = 0; i < m+n; i++){
-            nums1[i] = total[i];
-        }
         
         
     }
