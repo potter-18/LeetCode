915231933241451ba6195a83c3d0beb4546deaaa
potter-18 <96332841+potter-18@users.noreply.github.com>
2022-08-22 11:57:51 +0530

    Time: 10 ms (73.79%), Space: 7.4 MB (7.88%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -5,15 +5,19 @@ public:
             return false;
         }
         
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        
+        vector<int> freqS(26,0);
+        vector<int> freqT(26,0);
         
         for(int i = 0; i < s.size(); i++){
-            if(s[i] != t[i]){
-                return false;
-            }
+            freqS[s[i]-'a']++;
+            freqT[t[i]-'a']++;
         }
+        
+        for(int i = 0; i < s.size(); i++){
+            if(freqS[s[i]-'a'] != freqT[s[i]-'a'])
+                return false;     
+        }
+        
         return true;
     }
 };
