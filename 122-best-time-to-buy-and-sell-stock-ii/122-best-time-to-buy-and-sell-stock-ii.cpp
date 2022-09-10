b0612ef9793c240c2a6af43c5f49c6a882b57e5c
potter-18 <96332841+potter-18@users.noreply.github.com>
2022-09-10 16:10:29 +0530

    Time: 16 ms (32.68%), Space: 12.9 MB (90.02%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -7,21 +7,16 @@ public:
         int i;
         int minn = prices[0];
         for(i = 0; i < n-1; i++){
-            // cout << "minn: " << minn << " \n";
             if(prices[i] < prices[i+1]){
-                // cout << prices[i] << "--" << prices[i+1] << " \n";
                 continue;
             }
             else{
                 profit += (prices[i]-minn) ;
-                // cout << "profit: " << (prices[i]-minn) << "\n";
                 minn = prices[i+1];
             }
-            // cout << "\n";
         }
         
         profit += (prices[i]-minn) ;
-        
         
         return profit;
     }
