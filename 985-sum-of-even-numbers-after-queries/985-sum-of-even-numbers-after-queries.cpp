1ec28b584eb5b993a7b2ca187c15fe8d6cf72031
potter-18 <96332841+potter-18@users.noreply.github.com>
2022-09-21 11:50:15 +0530

    Time: 253 ms (21.43%), Space: 46.1 MB (38.69%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -5,20 +5,16 @@ public:
         int sum = 0;
         
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i]%2 == 0){
-                sum += nums[i];
-            }
+            if(nums[i]%2 == 0) sum += nums[i];
+            
         }
         
-        
         for(int i = 0; i < q.size(); i++){
-            if(nums[q[i][1]] % 2 == 0){
-                sum -= nums[q[i][1]];
-            }
+            if(nums[q[i][1]] % 2 == 0) sum -= nums[q[i][1]];
+        
             nums[q[i][1]] += q[i][0];
-            if(nums[q[i][1]] % 2 == 0){
-                sum += nums[q[i][1]];
-            }
+            if(nums[q[i][1]] % 2 == 0) sum += nums[q[i][1]];
+            
             ans.push_back(sum);
         }
         return ans;
