85b7b9e6a5773ce6ae18b1ec7ffb906e789ee110
potter-18 <96332841+potter-18@users.noreply.github.com>
2022-09-29 10:01:09 +0530

    Time: 163 ms (25.53%), Space: 31.9 MB (52.53%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
-    
-    static bool cmp(const pair<int,int>& a, const pair<int,int>& b){
-        if(a.second == b.second)
-            return a.first < b.first;
-        return a.second < b.second; 
-    }
-    
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n = arr.size();
-        vector<pair<int,int>> temp;
-        vector<int> ans;
+        stable_sort(begin(arr), end(arr), [x](const auto a, const auto b){
+            return abs(a - x) < abs(b - x); 
+        });
         
-        for(int i = 0; i < n; i++){
-            temp.push_back({arr[i],abs(arr[i]-x)});
-        }
-        
-        sort(temp.begin(), temp.end(), cmp);
-        
-        for(int i = 0; i < k; i++){
-            ans.push_back(temp[i].first);
-        }
-        sort(ans.begin(), ans.end());
-        return ans;
+        arr.resize(k);
+        sort(arr.begin(),arr.end());
+        return arr;
     }
 };
