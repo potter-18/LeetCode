98b264a48ce94d589db1719113024d3665df6a1f
potter-18 <96332841+potter-18@users.noreply.github.com>
2022-10-03 09:02:41 +0530

    Time: 400 ms (17.92%), Space: 95.5 MB (50.05%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int n = neededTime.size();
+        int l = 0, r = 1;
+        int timeReq = 0;
+        bool check = false;
+        
+        while(l < n && r < n && l < r){
+            // cout << "l,r: " << l << " " << r << "\n";
+            if(colors[l] == colors[r]){
+                if(neededTime[l] < neededTime[r]){
+                    timeReq += neededTime[l];
+                    // cout << "timeReq: " << timeReq << "\n";
+                }
+                else{
+                    timeReq += neededTime[r];
+                    // cout << "timeReq: " << timeReq << "\n";
+                    check = true;
+                    r++;
+                    continue;
+                }
+            }
+            if(check){
+                check = false;
+                l = r;
+                r++;
+            }else{
+                l++;
+                r++;
+            }
+            
+        }
+        
+        return timeReq;
+    }
+};
