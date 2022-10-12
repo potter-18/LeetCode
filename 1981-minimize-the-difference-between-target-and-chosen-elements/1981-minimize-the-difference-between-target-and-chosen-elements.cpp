0c0677585714be55a5bba101528fdf75182a0f4d
potter-18 <96332841+potter-18@users.noreply.github.com>
2022-10-12 10:26:28 +0530

    Time: 642 ms (86.29%), Space: 14.5 MB (83.71%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
+    int dp[71][4901];
     int diff = INT_MAX;
-    int checkMin(vector<vector<int>>& mat, int row, int target, int sum,  vector<vector<int>>& dp){
+    int checkMin(vector<vector<int>>& mat, int row, int target, int sum){
         if(row >= mat.size()){
             return abs(target-sum);
         }
         
-        if(dp[sum][row] != -1){
-            return dp[sum][row];
+        if(dp[row][sum] != -1){
+            return dp[row][sum];
         }
         
         int ans = INT_MAX;
         for(int i = 0; i < mat[0].size(); i++){
-            ans = min(checkMin(mat,row+1,target,sum+mat[row][i],dp),ans);
+            ans = min(checkMin(mat,row+1,target,sum+mat[row][i]),ans);
             if(ans == 0){
                 break;
             }
         }
         
-        return dp[sum][row] = ans;
+        return dp[row][sum] = ans;
     }
     
     int minimizeTheDifference(vector<vector<int>>& mat, int target){
         for(int i = 0; i < mat.size(); i++){
             sort(mat[i].begin(), mat[i].end());
         }
-        vector<vector<int>> dp(5000, vector<int>(80,-1));
-        return checkMin(mat,0,target,0,dp);
+        
+        for(int i = 0; i < 71; i++){
+            for(int j = 0; j < 4901; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return checkMin(mat,0,target,0);
     }
 };
