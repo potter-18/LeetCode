f2c2eb087181a02af8a4f1aaa49a758168c0e668
potter-18 <96332841+potter-18@users.noreply.github.com>
2022-10-19 12:24:46 +0530

    Time: 26 ms (48.98%), Space: 12.7 MB (39.56%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-    static bool cmp(pair<string,int>& a, pair<string,int>& b){
-        if(a.second == b.second) return a.first < b.first;
-        return a.second > b.second;
+    
+    
+    struct myComp {
+    public:
+        bool operator()(
+        pair<int, string> const& b,
+        pair<int, string> const& a)
+        const noexcept
+    {
+            if(a.first == b.first) return a.second < b.second;
+            return a.first > b.first;
+                
+        
     }
+};
     
     vector<string> topKFrequent(vector<string>& words, int k) {
         int n = words.size();
         unordered_map<string,int> freq;
-        vector<pair<string,int>> m;
+        priority_queue<pair<int,string>, vector<pair<int,string>>, myComp> m;
         
         for(int i = 0; i < n; i++) freq[words[i]]++;
         
-        for(auto it : freq) m.push_back({it.first,it.second});
+        for(auto it : freq) m.push({it.second,it.first});
         
-        sort(m.begin(),m.end(),cmp);
         vector<string> res;
-        for(auto it : m){
-            if(k > 0) res.push_back(it.first);
-            k--;
+        for(int i = 0; i < k; i++){ 
+            cout << m.top().first<< " " << m.top().second << "\n";
+            res.push_back(m.top().second);
+            m.pop();
         } 
-        
         return res;
     }
 };
