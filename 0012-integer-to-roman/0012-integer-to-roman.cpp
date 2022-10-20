8661c06606fa9ad279ccdf0714ad3f32b6a97952
potter-18 <96332841+potter-18@users.noreply.github.com>
2022-10-20 10:26:41 +0530

    Time: 17 ms (46.16%), Space: 6 MB (77.21%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        int temp = num;
-        vector<vector<string>> m(4);
-        m[0] = {"I","II","III","IV","V","VI","VII","VIII","IX"};
-        m[1] = {"X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        m[2] = {"C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        m[3] = {"M","MM","MMM"};
+        string m[4][9] = {{"I","II","III","IV","V","VI","VII","VIII","IX"},{"X","XX","XXX","XL","L","LX","LXX","LXXX","XC"},{"C","CC","CCC","CD","D","DC","DCC","DCCC","CM"},{"M","MM","MMM"}};
         string ans;
-        vector<int> ind;
-        for(int i = 0; i < 4, temp > 0; i++){
-            int curr = temp%10;
-            if(curr == 0){
-                ind.push_back(-1);
-            }else{
-                ind.push_back(curr-1);
-            }
-            
-            temp = temp/10;
+        int ind[4];
+        for(int i = 0; i < 4; i++){
+            int curr = num%10;
+            if(curr == 0) ind[i] = -1;
+            else ind[i] = curr-1;
+            num = num/10;
         }
-        for(int i = ind.size()-1; i >= 0; i--){
-            if(ind[i] == -1){
-                continue;
-            }
+        for(int i = 3; i >= 0; i--){
+            if(ind[i] == -1) continue;
             ans += m[i][ind[i]];
         }
         return ans;
