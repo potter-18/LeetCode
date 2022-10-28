3d3abaff2c7b9747c1e4536a7a3f30472a0aaf74
potter-18 <96332841+potter-18@users.noreply.github.com>
2022-10-28 09:10:03 +0530

    Time: 67 ms (64.67%), Space: 21.5 MB (25.94%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<string> str(strs.begin(),strs.end());
+        map<string,vector<string>> m;
+        for(int i = 0; i < str.size(); i++){
+            sort(str[i].begin(),str[i].end());
+            m[str[i]].push_back(strs[i]);
+        }
+        
+        sort(str.begin(),str.end());
+        vector<vector<string>> ans;
+        for(auto it : m){
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+        
+    }
+};
