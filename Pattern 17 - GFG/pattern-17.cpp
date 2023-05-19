9f89311e42dac2978a24efd9af8f27e1f6754c02
potter-18 <96332841+potter-18@users.noreply.github.com>
2023-05-19 15:49:30 +0530

    Added solution - LeetHub

---
+++ Pattern 17 - GFG/pattern-17.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        
+        for(int i = 0; i < n; i++) {
+            
+            for(int j = 0; j < n-i-1; j++) {
+                cout << " ";
+            }
+            char ch = 'A';
+            int brpt = (2*i+1)/2;
+            for(int j = 0; j < 2*i+1; j++) {
+                if(j < brpt) cout << ch++;
+                else cout << ch--;
+            }
+            
+            cout << "\n";
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
